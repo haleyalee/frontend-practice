5acb440d1dc3d067ef86dc630e3f5036a6fe807b
haleyalee <haleyalee@gmail.com>
2022-12-02 13:39:26 -0500

    Initial setup

---
+++ src/Routing.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+
+const Routing = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
+  )
+};
+
+export default Routing;
